d1bcc0243ea7b255fc848934e3734c8cc82e3aeb
bcroden <bcroden@uark.edu>
2015-03-07 09:28:22 -0600

    Added genre support to books package

--- Database Manager/src/com/team1/books/Book.java
+++ Database Manager/src/com/team1/books/Book.java
@@ -1,69 +1,73 @@
 package com.team1.books;
 
 /**
- * A basic container class for a book.
- * Feel free to add any additional fields you think are necessary or that I have missed.
+ * A basic container class for a book. Feel free to add any additional fields
+ * you think are necessary or that I have missed.
  * 
  * @author Brandon
  *
  */
 public class Book {
-    //Any string will do for a delimiter, but it should probably be different from the Query Delimiter
+    // Any string will do for a delimiter, but it should probably be different
+    // from the Query Delimiter
     public static final String DELIMITER = "@@@";
     public static final String HEADER = "BOOK_DATA";
-    
+
     public final String isbn;
     public final String title;
     public final String author;
     public final String publisher;
     public final String datePublished;
+    public final String genre;
 
-    public Book(String isbn, String title, String author, String publisher, String datePublished) {
+    public Book(String isbn, String title, String author, String publisher, String datePublished, String genre) {
         this.isbn = isbn;
         this.title = title;
         this.author = author;
         this.publisher = publisher;
         this.datePublished = datePublished;
+        this.genre = genre;
     }
-    
+
     public Book(String serializedString) {
         String[] data = serializedString.split(DELIMITER);
-        
+
         this.isbn = data[1];
         this.title = data[2];
         this.author = data[3];
         this.publisher = data[4];
         this.datePublished = data[5];
+        this.genre = data[6];
     }
-    
+
     @Override
     public String toString() {
-        return HEADER + DELIMITER + isbn + DELIMITER + title + DELIMITER + author + DELIMITER + publisher + DELIMITER + datePublished;
+        return HEADER + DELIMITER + isbn + DELIMITER + title + DELIMITER + author + DELIMITER + publisher + DELIMITER + datePublished
+                + DELIMITER + genre;
     }
-    
-    //Basic main for testing purposes
+
+    // Basic main for testing purposes
     public static void main(String[] args) throws InvalidISBNException {
         Book book = BookFinder.getBookFromGoogle("054792822X");
-        
-        System.out.println("Unserialized Book.");
+
         System.out.println("Isbn:           " + book.isbn);
         System.out.println("Title:          " + book.title);
+        System.out.println("Genre:          " + book.genre);
         System.out.println("Author:         " + book.author);
         System.out.println("Publisher:      " + book.publisher);
         System.out.println("Date Published: " + book.datePublished + "\n");
-        
+
         String serializedBook = book.toString();
-        
-        System.out.println("Serialized Book.");
-        System.out.println(serializedBook + "\n");
-        
+
+        System.out.println("Serialized Book: " + serializedBook + "\n");
+
         Book book2 = new Book(serializedBook);
-        
-        System.out.println("Deserialized Book.");
+
         System.out.println("Isbn:           " + book2.isbn);
         System.out.println("Title:          " + book2.title);
+        System.out.println("Genre:          " + book2.genre);
         System.out.println("Author:         " + book2.author);
         System.out.println("Publisher:      " + book2.publisher);
-        System.out.println("Date Published: " + book2.datePublished + "\n");
+        System.out.println("Date Published: " + book2.datePublished);
     }
 }
--- Database Manager/src/com/team1/books/BookFinder.java
+++ Database Manager/src/com/team1/books/BookFinder.java
@@ -10,25 +10,30 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
- * This class will lookup a book by its ISBN on Google and return a Book object representing that data.
+ * This class will lookup a book by its ISBN on Google and return a Book object
+ * representing that data.
  * 
  * @author Brandon
  *
  */
 public class BookFinder {
     private static final String GOOGLE_API_URL = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
-    
+
     /**
-     * Will return a Book containing information found on Google for the given ISBN
+     * Will return a Book containing information found on Google for the given
+     * ISBN
      * 
-     * @param isbn -  The ISBN of the book you want to look up
+     * @param isbn
+     *            - The ISBN of the book you want to look up
      * @return A book containing the data for the specified ISBN
-     * @throws InvalidISBNException If the entered ISBN is of improper length, or is not registered on Google
+     * @throws InvalidISBNException
+     *             If the entered ISBN is of improper length, or is not
+     *             registered on Google
      */
     public static Book getBookFromGoogle(String isbn) throws InvalidISBNException {
         if (isbn.length() != 10 && isbn.length() != 13)
             throw new InvalidISBNException("ISBN length not equal to 10 or 13.");
-        
+
         // Get the entire JSON book object
         JSONObject jsonBook = null;
         try {
@@ -36,67 +41,72 @@ public class BookFinder {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        if(jsonBook != null && jsonBook.has("totalItems") && jsonBook.getInt("totalItems") == 0)
-            throw new InvalidISBNException("ISBN " + isbn  + " not found.");
-        
+
+        if (jsonBook != null && jsonBook.has("totalItems") && jsonBook.getInt("totalItems") == 0)
+            throw new InvalidISBNException("ISBN " + isbn + " not found.");
+
         // Make several references to important spots in the JSON structure
         JSONArray items = (jsonBook != null && jsonBook.has("items")) ? jsonBook.getJSONArray("items") : null;
         JSONObject volumeInfo = (items != null && items.length() != 0) ? items.getJSONObject(0).getJSONObject("volumeInfo") : null;
         JSONArray authors = (volumeInfo != null && volumeInfo.has("authors")) ? volumeInfo.getJSONArray("authors") : null;
+        JSONArray categories = (volumeInfo != null && volumeInfo.length() != 0) ? volumeInfo.getJSONArray("categories") : null;
 
-        // Get the data from the JSON object, If an entry does not exist, return ""
+        // Get the data from the JSON object, If an entry does not exist, return
+        // ""
         String title = (volumeInfo != null && volumeInfo.has("title")) ? volumeInfo.getString("title") : "";
         String author = (authors != null && authors.length() != 0) ? authors.getString(0) : "";
-        String publisher =  (volumeInfo != null && volumeInfo.has("publisher")) ? volumeInfo.getString("publisher") : "";
-        String datePublished =  (volumeInfo != null && volumeInfo.has("publishedDate")) ? volumeInfo.getString("publishedDate") : "";
-        
-        //Return the book
-        return new Book(isbn, title, author, publisher, datePublished);
+        String publisher = (volumeInfo != null && volumeInfo.has("publisher")) ? volumeInfo.getString("publisher") : "";
+        String datePublished = (volumeInfo != null && volumeInfo.has("publishedDate")) ? volumeInfo.getString("publishedDate") : "";
+        String genre = (categories != null & categories.length() != 0) ? categories.getString(0) : "";
+
+        // Return the book
+        return new Book(isbn, title, author, publisher, datePublished, genre);
     }
-    
+
     private static JSONObject getJSONObjectFromGoogle(String isbn) throws IOException {
         URL google = new URL(GOOGLE_API_URL + isbn);
-        
+
         URLConnection connection = google.openConnection();
         BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String total = "";
         String inputLine;
-        
+
         while ((inputLine = input.readLine()) != null)
             total = total.concat(inputLine);
         input.close();
-        
+
         return new JSONObject(total);
     }
-    
-    //Simple main for testing purposes
+
+    // Simple main for testing purposes
     public static void main(String[] args) throws InvalidISBNException {
         Book book;
-        
-        //Test if ISBN 10 works
+
+        // Test if ISBN 10 works
         book = BookFinder.getBookFromGoogle("054792822X");
         System.out.println("Isbn:           " + book.isbn);
         System.out.println("Title:          " + book.title);
+        System.out.println("Genre:          " + book.genre);
         System.out.println("Author:         " + book.author);
         System.out.println("Publisher:      " + book.publisher);
         System.out.println("Date Published: " + book.datePublished + "\n");
-        
-        //Test if ISBN 13 works
+
+        // Test if ISBN 13 works
         book = BookFinder.getBookFromGoogle("9780345453747");
         System.out.println("Isbn:           " + book.isbn);
         System.out.println("Title:          " + book.title);
+        System.out.println("Genre:          " + book.genre);
         System.out.println("Author:         " + book.author);
         System.out.println("Publisher:      " + book.publisher);
         System.out.println("Date Published: " + book.datePublished);
-        
-        //Test if exception is thrown for ISBNs of improper length
+
+        // Test if exception is thrown for ISBNs of improper length
         try {
             book = BookFinder.getBookFromGoogle("123456789");
         } catch (InvalidISBNException e) {
             e.printStackTrace();
         }
-        //Test if exception is thrown for invalid ISBNs
+        // Test if exception is thrown for invalid ISBNs
         try {
             book = BookFinder.getBookFromGoogle("1234567890");
         } catch (InvalidISBNException e) {
