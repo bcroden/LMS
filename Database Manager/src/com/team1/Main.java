c5a3cd3642c576f4ccfa0a0f439c45a32c6b4fce
cadeg <chgatewo@uark.edu>
2015-03-09 01:33:03 +0000

    Uploading database wrapper that accomplishes milestone, will discuss what else needs adding

--- Database Manager/src/com/team1/Main.java
+++ Database Manager/src/com/team1/Main.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import com.team1.network.TCPServer;
 
 import com.team1.db.Dbwrapper;
+import com.team1.books.*;
 
 public class Main
 {
@@ -26,13 +27,36 @@ public class Main
         thread.interrupt();
         thread.join();
         
-        //testing for database connection
+        
+        //This section is TO DELETE for later
+        //for now: testing for database connection
         Dbwrapper db = Dbwrapper.getInstance();
+        
+        //Example of getting query results and printing them
+        //Single result
+        System.out.println("\nSearch for book with ISBN: 1234\n");
+        Book book = db.SearchISBN("1234");
+        System.out.println(book.toString());
+        
+        //Multiple results
+        System.out.println("\nSearch for all books with Author: Me\n");
         ArrayList list = new ArrayList();
         list = db.SearchAuthor("Me");
         int size = list.size();
         for(int i = 0; i < size; i++){
             System.out.println(list.get(i).toString());
         }
+        System.out.println(book.toString());
+        
+        
+        
+        //ISBNs are unique ids so you can't input these more than once
+        Adding a new book to the repo
+        Book bookthing = BookFinder.getBookFromGoogle("054792822X");
+        db.addBook(bookthing);
+        
+        Book book2 = BookFinder.getBookFromGoogle("0439136369");
+        db.addBook(book2);
+        
     }
 }
--- Database Manager/src/com/team1/db/Dbwrapper.java
+++ Database Manager/src/com/team1/db/Dbwrapper.java
@@ -1,10 +1,10 @@
-package com.team1.db;       //Alex changed 3-7-15 this to get everything to compile
+package com.team1.db;
 
 import java.sql.*;
 import java.util.ArrayList;
 
 //get book objects
-import com.team1.books.Book;
+import com.team1.books.*;
 
 
 
@@ -15,7 +15,7 @@ public class Dbwrapper {
 	
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql//127.0.0.1:3306/LMS";
-	
+	//Can adjust these and they don't have to be hard coded
 	static final String username = "cadeg";
 	static final String pass = "";
 	
@@ -23,8 +23,6 @@ public class Dbwrapper {
 	//and return the result to the request
 	//from authentication
 	
-	//I'm thinking we should return arraylists filled with book objects
-	//Then presenting lists of data will be really easy in keeping stuff together
 	
 	private Dbwrapper(){
 		//get driver and connection
@@ -32,7 +30,6 @@ public class Dbwrapper {
 			Class.forName(JDBC_DRIVER).newInstance();
 			//connect
 			con = DriverManager.getConnection("jdbc:mysql://localhost/LMS", "cadeg", "");
-			System.out.println("Connected if we get here");
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -45,8 +42,7 @@ public class Dbwrapper {
 			
 		}
 		
-	public void query() throws SQLException
-	{
+	public void query() throws SQLException{
 		Statement stmt = con.createStatement();
 		String sql = "Select * From book ISBN";
                 ResultSet result =  stmt.executeQuery(sql);
@@ -56,13 +52,59 @@ public class Dbwrapper {
                 }
 	}
 	
-	public void addBook(Book book)throws SQLException
-	{
-		Statement stmt = con.createStatement();
+	//--------------------------------------------------------------------------
+	
+	//Function for adding a new book to the repo
+	//--------------------------------------------------------------------------
+	public void addBook(Book book)throws SQLException{
+		String tempp = book.publisher;
+		String temppd = book.datePublished;
+		String tempt = book.title;
 		
+		System.out.println("Contents: " + book.isbn + "', '" + book.title + "', '" + book.author +
+					 "', ' Genre: " + book.genre + "', ' Publisher: " + book.publisher + "', '" + book.datePublished);
+		
+		if(tempp == null || tempp.equals("")){
+			System.out.println("Null publisher");
+		Statement stmt = con.createStatement();
+		String sql = "INSERT INTO book " +
+					 "(isbn, title, author, genre, publishdate, likes, dislikes, copies) " + 
+					 "VALUES ('" + book.isbn + "', '" + book.title + "', '" + book.author +
+					 "', '" + book.genre + "', '" + book.datePublished + 
+					 "', '" + 0 + "', '" + 0 + "', '" + 0 + "')";
+		stmt.executeUpdate(sql);
+		}
+		else if(temppd == null || temppd.equals("")){
+			System.out.println("Null publisherdate");
+		Statement stmt = con.createStatement();
+		String sql = "INSERT INTO book " +
+					 "(isbn, title, author, genre, publisher, likes, dislikes, copies) " + 
+					 "VALUES ('" + book.isbn + "', '" + book.title + "', '" + book.author +
+					 "', '" + book.genre + "', '" + book.publisher + "', '" + 0 + "', '" + 0 + "', '" + 0 + "')";
+		stmt.executeUpdate(sql);
+		}
+		else if(tempt == null || tempt.equals("")){
+			System.out.println("Null title");
+			System.out.println("The ISBN did not provide a title, no database entry created.");
+		}
+		else{
+			System.out.println("No null");
+		Statement stmt = con.createStatement();
+		String sql = "INSERT INTO book " +
+					 "(isbn, title, author, genre, publisher, publishdate, likes, dislikes, copies) " + 
+					 "VALUES ('" + book.isbn + "', '" + book.title + "', '" + book.author +
+					 "', '" + book.genre + "', '" + book.publisher + "', '" + book.datePublished + 
+					 "', '" + 0 + "', '" + 0 + "', '" + 0 + "')";
+		stmt.executeUpdate(sql);
+		}
 	}
+	//Functions for updating book info (likes dislikes and copies)
+	
+	
+	//--------------------------------------------------------------------------
 	
 	//These functions should return all books that match the criteria
+	//--------------------------------------------------------------------------
     public ArrayList<Book> SearchAuthor(String author)throws SQLException
     {
     	Statement stmt = con.createStatement();
@@ -102,11 +144,188 @@ public class Dbwrapper {
     	}
     	return list;
     }
-        
-        //Static testing - won't actually be here for final product
-	public static void main(String args[]) throws SQLException
-	{
-		Dbwrapper temp = getInstance();
-		temp.query();
-	}
+    
+    public ArrayList<Book> SearchPublisher(String publisher)throws SQLException
+    {
+    	Statement stmt = con.createStatement();
+    	String sql = "SELECT * FROM book WHERE publisher = '" + publisher + "'";
+    	ResultSet result = stmt.executeQuery(sql);
+    	ArrayList list = new ArrayList<Book>();
+    	while(result.next()){
+    		Book book = new Book(
+    			result.getString("ISBN"),
+    			result.getString("title"),
+    			result.getString("author"),
+    			result.getString("publisher"),
+    			result.getString("publishdate"),
+    			result.getString("genre"));
+    			
+    		list.add(book);
+    	}
+    	return list;
+    }
+	//date 1 is an earlier date than date2, its possible to error check here
+	//but we can fix this on input
+    public ArrayList<Book> SearchDate(String date, String date2)throws SQLException
+    {
+    	Statement stmt = con.createStatement();
+    	//date has to be in the format yyyy
+    	//This can be handled on front end of searches though
+    	String sql = "SELECT * FROM book WHERE publishdate between '" + date + "' and '" + date2 + "'";
+    	ResultSet result = stmt.executeQuery(sql);
+    	ArrayList list = new ArrayList<Book>();
+    	while(result.next()){
+    		Book book = new Book(
+    			result.getString("ISBN"),
+    			result.getString("title"),
+    			result.getString("author"),
+    			result.getString("publisher"),
+    			result.getString("publishdate"),
+    			result.getString("genre"));
+    			
+    		list.add(book);
+    	}
+    	return list;
+    }
+    
+    public ArrayList<Book> SearchGenre(String genre)throws SQLException
+    {
+    	Statement stmt = con.createStatement();
+    	String sql = "SELECT * FROM book WHERE genre = '" + genre + "'";
+    	ResultSet result = stmt.executeQuery(sql);
+    	ArrayList list = new ArrayList<Book>();
+    	while(result.next()){
+    		Book book = new Book(
+    			result.getString("ISBN"),
+    			result.getString("title"),
+    			result.getString("author"),
+    			result.getString("publisher"),
+    			result.getString("publishdate"),
+    			result.getString("genre"));
+    			
+    		list.add(book);
+    	}
+    	return list;
+    }
+    //End of queries returning book lists   
+    //--------------------------------------------------------------------------
+	//These functions should return singlar books
+	//--------------------------------------------------------------------------
+	public Book SearchISBN(String ISBN)throws SQLException
+    {
+    	Statement stmt = con.createStatement();
+    	String sql = "SELECT * FROM book WHERE isbn = '" + ISBN + "'";
+    	ResultSet result = stmt.executeQuery(sql);
+    	String isbn = "";
+    	String title = "";
+    	String author = "";
+    	String publisher = "";
+    	String date = "";
+    	String genre = "";
+    	while(result.next()){
+    			isbn = result.getString("ISBN");
+    			title = result.getString("title");
+    			author = result.getString("author");
+    			publisher = result.getString("publisher");
+    			date = result.getString("publishdate");
+    			genre = result.getString("genre");
+    		}
+    	Book book = new Book(isbn, title, author, publisher, date, genre);
+    	return book;
+    }
+    
+    public Book SearchID(int id)throws SQLException
+    {
+    	Statement stmt = con.createStatement();
+    	String sql = "SELECT * FROM book WHERE id = '" + id + "'";
+    	ResultSet result = stmt.executeQuery(sql);
+    	String isbn = "";
+    	String title = "";
+    	String author = "";
+    	String publisher = "";
+    	String date = "";
+    	String genre = "";
+    	while(result.next()){
+    			isbn = result.getString("ISBN");
+    			title = result.getString("title");
+    			author = result.getString("author");
+    			publisher = result.getString("publisher");
+    			date = result.getString("publishdate");
+    			genre = result.getString("genre");
+    		}
+    	Book book = new Book(isbn, title, author, publisher, date, genre);
+    	return book;
+    }
+    //end of single returning book queries
+    //--------------------------------------------------------------------------
+    
+    //User related queries
+    //--------------------------------------------------------------------------
+    	public void addUser(String user, String pass, String email, String firstname, String lastname, int enotify)throws SQLException{
+		Statement stmt = con.createStatement();
+		String sql = "INSERT INTO user " +
+					 "(username, password, fname, lname, email, notify) " + 
+					 "VALUES ('" + user + "', '" + pass + "', '" + firstname +
+					 "', '" + lastname + "', '" + email + "', '" + enotify + "')";
+		stmt.executeUpdate(sql);
+    	}
+    	
+    	public String getPass(String user){
+    		Statement stmt = con.createStatement();
+    		String sql = "SELECT password FROM user WHERE username = '" + user + "'";
+    		ResultSet result = stmt.executeQuery(sql);
+    		String pass;
+    		while(result.next()){
+    			pass = result.getString(password);
+    		}
+    		return pass;
+    	}
+    	
+    	
+    
+    //--------------------------------------------------------------------------
+    
+    //Librarian related queries
+    //--------------------------------------------------------------------------
+    public void CheckOut(String isbn){
+    	Statement stmt = con.createStatement();
+    	String sql = "SELECT copies FROM BOOK WHERE isbn = '" + isbn + "'";
+    	ResultSet result = stmt.executeQuery(sql);
+    	int copies;
+    	while(result.next()){
+    		copies = result.getInt(copies);
+    	}
+    	copies--;
+    	sql = "UPDATE BOOK copies = '" + copies + "' WHERE isbn = '" + isbn + "'";
+    	stmt.executeUpdate(sql);
+    	
+    	//Book related to isbn will be added to user who checked it out
+    	
+    }
+    
+     public void CheckIn(String isbn){
+    	Statement stmt = con.createStatement();
+    	String sql = "SELECT copies FROM BOOK WHERE isbn = '" + isbn + "'";
+    	ResultSet result = stmt.executeQuery(sql);
+    	int copies;
+    	while(result.next()){
+    		copies = result.getInt(copies);
+    	}
+    	copies++;
+    	sql = "UPDATE BOOK copies = '" + copies + "' WHERE isbn = '" + isbn + "'";
+    	stmt.executeUpdate(sql);
+    	
+    	//book will be removed from user checked out list
+    	//and added to user history
+    }
+    
+     public void SetCopies(String isbn, int copies){
+    	Statement stmt = con.createStatement();
+    	sql = "UPDATE BOOK copies = '" + copies + "' WHERE isbn = '" + isbn + "'";
+    	stmt.executeUpdate(sql);
+    }
+    //--------------------------------------------------------------------------
+    
+    //Administrative queries
+    //Not sure what exactly we want from here
 }
