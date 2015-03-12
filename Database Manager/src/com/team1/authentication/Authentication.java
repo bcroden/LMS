7fd1b7dbd8aae05c8232820a98fa7dd0e0760c59
rxm033 <rxm033@email.uark.edu>
2015-03-12 00:37:07 -0500

    First version

--- Database Manager/src/com/team1/authentication/Authentication.java
+++ Database Manager/src/com/team1/authentication/Authentication.java
@@ -1,5 +1,11 @@
 package com.team1.authentication;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Random;
+
+import com.team1.db.Dbwrapper;
+import com.team1.formatting.LibrarianQuery;
 import com.team1.formatting.LoginQuery;
 import com.team1.formatting.Query;
 
@@ -10,8 +16,10 @@ public class Authentication {
 	private static boolean permission;
 	private static String password;
 	private static String username;
-	private static String userLevel;
+	private static int userLevel;
 	private static int Id; 
+	private static int Return;
+	static String temp77;
 	
 	//Constructor
 	public Authentication(Query query) 
@@ -19,60 +27,82 @@ public class Authentication {
 		// TODO
 	}
 	
-	public static boolean authenticate(Query query)
+	//Returns 0 if an ID is not found. Returns an ID if it is
+	public int authenticate(Query query)
 	{
-		type = query.queryType;
-		if(type == "LoginQuery") //Consider using query instanceof LoginQuery -Brandon
+		 HashMap<Integer, Integer> map = new HashMap<>();
+		
+		type = Query.queryType;
+		if(query instanceof LoginQuery) 
 		{
+			//Dbwrapper myDatabase = Dbwrapper.getInstance();
 			password = ((LoginQuery)query).password; //Modified to make it compile -Brandon
 			username = ((LoginQuery)query).userName;
-			// Id = however I am getting the ID
-			Id = 12345678;
-			if(searchMap(Id) == true)
-			{
-				permission = true;
-				return permission;
+			userLevel = 2;
+		/*	try {
+				System.out.println(username);
+				//userLevel = myDatabase.getAuthorization(username, password);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			else
-			{
-				//userLevel = Database(username, password)
-				//TODO check if(Type <= userLevel in terms of access granted
-				/* if(true)
+		*/	
+				if(userLevel == 0)
 				   {
-                      //randomly generate 8 digit number
-                      //append 0,1,2 in correspondance with userLevel
-                      //store in hashMap using addMap				 
+				   		Return = 0;
+				   		return Return;
 				   }
-				 
-				 */
-				permission = true;
-				return permission;
+				
+				else if (userLevel == 1 || userLevel == 2 || userLevel == 3)
+				   {
+					  int LocalId = 0;
+					//  int temp99 = map.get(62045572);
+					 // System.out.println("The key should be: " + temp99);
+                      Random n = new Random();
+                      LocalId = n.nextInt(89999999) + 10000000;
+      //I comented out this line the 2nd time I ran the code and it gave me a null pointer exception
+                      map.put(11111111, userLevel);
+                      int temp99 = map.get(11111111);
+                      System.out.println("this is: temp99: " + LocalId);
+                      Return = LocalId;
+                      return Return;
+				   }
+				   else
+				   {
+				   System.out.print("Something has gone terribly wrong with authentication");
+				   return 0;
+				   }				   
+				 				
 			}
-		}
-//		else{return false;}
+		
 		
 		else if(type == "PatronInfoQuery")
 		{
-			//TODO if ID is found, return true
-		    return false;
+			//Use serachMap(username, ID)
+			//Get first digit and compare with user level
+			//if firstDigit <= userlevel return true
+		    return 0;
 		}
-		else{return false;}
+		else{return 0;}
+		}
 	
 		//TODO AdminQuery
 		//TODO CheckInBookQuery
 		//TODO CheckOutBookQuery
 		//TODO LibrarianInfoQuery
 		//TODO LibrarianQuery
-	}
+		
 	
-	public static void addMap()
-	{
-		//TODO make hash map and add stuff
-	}
 	
-	public static boolean searchMap(int ID)
-	{
-		//TODO hash map search
-		return true;
-	}
+
+		public static void main(String[] args)
+		{
+			LoginQuery dummy = new LoginQuery("11111111", "bob", "pass");
+			System.out.println("Hello there");
+			Authentication person = new Authentication(dummy);
+			int temp88;
+			temp88 = person.authenticate(dummy);
+			System.out.println("temp 88 is: " + temp88);
+			
+		}
 }
