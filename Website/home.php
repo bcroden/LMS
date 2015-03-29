972b15b9cb06b0632df1b4835999d270123c72ad
Cade <cade@cade-VirtualBox.(none)>
2015-03-29 01:33:44 -0500

    Added search types to website and a scrollable list for the returned queries

--- Website/home.php
+++ Website/home.php
@@ -6,10 +6,15 @@
         <p>Welcome to the Library Management System</p>
         <p>
             <form method="post" action="/search.php">
+				<select name="type">
+					<option value="isbn">ISBN</option>
+					<option value="title">Title</option>
+					<option value="author">Author</option>
+					<option value="genre">Genre</option>
+				</select>
                 Search for a book: <input type="text" name="search">
                 <input type="submit" value="Search">
             </form>
         </p>
     </body>
 </html>
-
--- Website/search.php
+++ Website/search.php
@@ -1,18 +1,100 @@
-<?php
+<html>
+<head>
 
+<style>
+div.scroll {
+	background-color: D6D6D6;
+    height: 200px;
+    overflow: auto;
+}
+
+//might make another div for clicking sub items
+
+</style>
+
+</head>
+<body>
+<?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 include 'DBwrapper.php';
 
-echo "Connecting to database <br>";
+echo "Connecting to database <br><br>";
 connect();
-echo "Searching by title<br>";
-	$result = queryTitle($_POST['search']);
+	$type = $_POST['type'];
 
-	while($row = mysqli_fetch_array($result)){
-		echo "id: " . $row["id"] . "<br>";
+	echo "Searching by: " . $type . "<br>";
+
+	if($type === "isbn"){
+        $result = queryISBN($_POST['search']);
+        echo "<div class=scroll>";
+		if($result === -1){
+            echo "Search returned no results...<br>";
+        }
+        else{
+            while($row = mysqli_fetch_array($result)){
+                //The return can be adjusted as we feel necessary
+                echo "Title: " . $row["title"] . "<br>Author: " . $row["author"] .
+                "<br>Copies available: " . $row["copiesin"] . "<br><br>";
+            }
+		}
+        echo "</div>";
+    }
+
+	else if($type === "title"){
+		$result = queryTitle($_POST['search']);
+		echo "<div class=scroll>";
+		if($result === -1){
+            echo "Search returned no results...<br>";
+        }
+        else{
+            while($row = mysqli_fetch_array($result)){
+                //The return can be adjusted as we feel necessary
+                echo "Title: " . $row["title"] . "<br>Author: " . $row["author"] .
+                "<br>Copies available: " . $row["copiesin"] . "<br><br>";
+            }
+        }
+			//I want to make each item clickable to view more information
+			//on that specific book but it is looking very complex to do
+			//with the list in this format
+			//I'll look into changing it but adding a link at the bottom
+			//of each item would solve it too
+		echo "</div>";
 	}
 
+	else if($type === "author"){
+        $result = queryAuthor($_POST['search']);
+        echo "<div class=scroll>";
+		if($result === -1){
+            echo "Search returned no results...<br>";
+        }
+        else{
+            while($row = mysqli_fetch_array($result)){
+                //The return can be adjusted as we feel necessary
+                echo "Title: " . $row["title"] . "<br>Author: " . $row["author"] .
+                "<br>Copies available: " . $row["copiesin"] . "<br><br>";
+            }
+		}
+        echo "</div>";
+    }
+
+	else if($type === "genre"){
+        $result = queryGenre($_POST['search']);
+        echo "<div class=scroll>";
+		if($result === -1){
+			echo "Search returned no results...<br>";
+		}
+		else{
+        	while($row = mysqli_fetch_array($result)){
+            	//The return can be adjusted as we feel necessary
+            	echo "Title: " . $row["title"] . "<br>Author: " . $row["author"] .
+            	"<br>Copies available: " . $row["copiesin"] . "<br><br>";
+        	}
+		}
+        echo "</div>";
+    }
 ?>
 
+</body>
+</html>
