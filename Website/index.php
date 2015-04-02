d233e71890e86326a495353c69798bc6bfa5830b
alex-anderson <opiemike613@yahoo.com>
2015-04-02 13:30:47 -0500

    Basic Website Accounts

--- Website/index.php
+++ Website/index.php
@@ -1,40 +1,7 @@
-<?php
-
-    $host = "cadeg-lms-1330962";
-    $user = "cadeg";
-    $pass = "";
-    //database not yet created, placeholder variable/name
-    $db = "LMS";
-    $port = "3306";
-
-    //setup connection to be referenced
-    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error);
-    
-    if ($connection->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
-    
-    echo "Pre query" . "<br>";
-    
-    
-    //This is how queries are performed in PHP
-    //We can just concatonate which variables we want to search
-    //This will accomplish the search for books by title or whatever
-    $sql = "SELECT id, isbn, title FROM book WHERE title = '" . $_POST['search'] . "'";
-    $result = $connection->query($sql);
-    
-    
-    
-    
-    //Printing results
-    if($result->num_rows > 0){
-        //output data
-        while($row = $result->fetch_assoc()){
-            echo "id: " . $row["id"] . " isbn: " . $row["isbn"] . " title: " . $row["title"] . "<br>";
-        }
-    }
-    else{
-        echo "Error or something...I don't know <br>";
-    }
-    //adjust query for testing
-?>
+<html>
+   <body>
+      <a href="login.php">Log In</a>
+      <br/>
+      <a href="create_account.php">Create New Account</a>
+   </body>
+</html>
