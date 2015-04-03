1402be2737113a33a782948a1f99a225e11be36f
bcroden <bcroden@uark.edu>
2015-04-02 23:50:09 -0500

    more

--- Workstation/src/main/java/com/team1/formatting/responses/CheckOutBookResponse.java
+++ Workstation/src/main/java/com/team1/formatting/responses/CheckOutBookResponse.java
@@ -1,12 +1,29 @@
 package com.team1.formatting.responses;
 
+import java.util.ArrayList;
+
+import com.team1.books.Book;
 
 public class CheckOutBookResponse extends Response
 {
+	public static final String bookBreak = ";$;";
+    public String userName;
+    public String fines;
+    public ArrayList<Book> books = null;
     
-    public CheckOutBookResponse(boolean wasSuccessful, String sessionID)
+    public CheckOutBookResponse(boolean wasSuccessful, String sessionID, String userName, String fines, int numBooks, String strBooks)
     {
         super(wasSuccessful, sessionID);
+        this.userName = userName;
+        this.fines = fines;
+        
+        books = new ArrayList<Book>();
+        
+        String[] bookList = strBooks.split(bookBreak);
+        for (int i = 0; i < numBooks; i++)
+        {
+            books.add(new Book(bookList[i]));
+        }
     }
     
     public CheckOutBookResponse() 
@@ -17,10 +34,21 @@ public class CheckOutBookResponse extends Response
 	//Override of toString. Method to return the object information in the form of a string.
     @Override
     public String toString() {
-        String s;
+    	String s;
+        
+        int numBooks = 0;
+        numBooks = books.size();
+        
         if (wasSuccessful) s = "true";
         else s = "false";
-        String msg = "CheckOutBookResponse" + DELIMITER + s + DELIMITER + sessionID;
+        String msg = "CheckOutBookResponse" + DELIMITER + s + DELIMITER + sessionID + DELIMITER + userName + DELIMITER + fines + DELIMITER + numBooks + DELIMITER;
+        
+        for (int i = 0; i < numBooks; i++)
+        {
+            msg = msg.concat(books.get(i).getSerialized());
+            msg = msg.concat(bookBreak);
+        }
+        
         return msg;
     }
 }
