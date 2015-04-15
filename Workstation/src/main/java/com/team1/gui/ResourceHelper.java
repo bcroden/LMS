e166f782a4177dfa71bde1d6e0b590d64ce9627c
rxm033 <rxm033@email.uark.edu>
2015-04-14 22:26:19 -0500

    BUTTON PICTURESgit add -Agit add -Agit add -Agit add -Agit add -Agit add -A

--- Workstation/src/main/java/com/team1/gui/ResourceHelper.java
+++ Workstation/src/main/java/com/team1/gui/ResourceHelper.java
@@ -6,6 +6,10 @@ public class ResourceHelper {
 	public static final String ICON_PATH_ADD_USER = 	IMAGE_FOLDER + "/flat-ui-icons-free/usericon16g.png";
     public static final String ICON_PATH_BOOK = 		IMAGE_FOLDER + "/double-j-design/book128.png";
     public static final String ICON_PATH_DEFAULT_BOOK = IMAGE_FOLDER + "/double-j-design/book128.png";
+    public static final String ICON_PATH_PLUS = 	    IMAGE_FOLDER + "/double-j-design/add.png";
+    public static final String ICON_PATH_MINUS = 	    IMAGE_FOLDER + "/double-j-design/minus_circle_green.png";
+    public static final String ICON_PATH_ARROW = 		IMAGE_FOLDER + "/double-j-design/arrow_up.png";
+    public static final String ICON_PATH_DOLLAR = 		IMAGE_FOLDER + "/double-j-design/currency_dollar_green.png";
     public static final String ICON_PATH_FINES = 		IMAGE_FOLDER + "/aiga/customdollar16.png";
     public static final String ICON_PATH_FRAME_ICON = 	IMAGE_FOLDER + "/double-j-design/book32.png";
     public static final String ICON_PATH_LOGOUT = 		IMAGE_FOLDER + "/flat-ui-icons-free/xicon16.png";
@@ -13,4 +17,5 @@ public class ResourceHelper {
     public static final String ICON_PATH_PASSWORD = 	IMAGE_FOLDER + "/flat-ui-icons-free/lockicon16.png";
     public static final String ICON_PATH_REM_USER = 	IMAGE_FOLDER + "/flat-ui-icons-free/usericon16r.png";
     public static final String ICON_PATH_SEARCH = 		IMAGE_FOLDER + "/search-icons/searchicon128.png";
+
 }
