0a5a5514a9e7d133a402428a2df2c180e77f69c9
alex-anderson <opiemike613@yahoo.com>
2015-04-14 21:08:09 -0500

    Update include paths

--- Website/log_button.php
+++ Website/log_button.php
@@ -11,35 +11,25 @@
    if(isset($_SESSION["init"])) {
 ?>
 
-<<<<<<< HEAD
-<form style="position:absolute;top:10px;right:30px;" action="/log_button.php" method="post">
-=======
 <form style="position:absolute;top:10px;right:100px;" action="log_button.php" method="post">
->>>>>>> cf3d1eebe5c228c014e0ebf3ec5e932a108364cc
    <input type="hidden" name="logout" value="yes"/>
    <input type="hidden" name="return" value="<?= $_SERVER['PHP_SELF'] ?>"/>
-   <a href="/accounts/view_account.php"><?= $_SESSION["usernm"] ?></a> <input type="submit" value="logout"/>
+   <a href="view_account.php"><?= $_SESSION["usernm"] ?></a> <input type="submit" value="logout"/>
 </form>
 
 <?php
    } else {
 ?>
 
-<<<<<<< HEAD
-<form style="position:absolute;top:10px;right:30px;" action="<?= '/accounts/login.php' ?>" method="post">
-=======
 <form style="position:absolute;top:10px;right:100px;" action="<?= 'login.php' ?>" method="post">
->>>>>>> cf3d1eebe5c228c014e0ebf3ec5e932a108364cc
    <input type="submit" value="Login"/>
 </form>
 
 <?php
    }
 ?>
-<<<<<<< HEAD
-=======
 
 <form style="position:absolute;top:10px;right:10px;" action="index.php" method="post">
     <input type="submit" value="Home"/>
 </form>
->>>>>>> cf3d1eebe5c228c014e0ebf3ec5e932a108364cc
+
