b2fc7d7ddfadc3df368abe7b200542af13e8deed
rxm033 <rxm033@email.uark.edu>
2015-04-26 22:58:18 -0500

    Forgot to remove the hack

--- Workstation/src/main/java/com/team1/gui/LoginWindow.java
+++ Workstation/src/main/java/com/team1/gui/LoginWindow.java
@@ -141,7 +141,7 @@ public class LoginWindow extends LMSWindow {
 				
 				if(r == null) {
 					errorMessage.setText(ERROR_MESSAGE_TEXT);
-					controller.showMainWindow();
+					//controller.showMainWindow();
 				}
 				else {
 					System.out.println("Before response");
