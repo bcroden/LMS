3a0954512301301250668a64551a738fa941bd9b
alex-anderson <opiemike613@yahoo.com>
2015-04-28 17:31:55 -0500

    Add dropdown menu back to Website

--- Website/log_button.php
+++ Website/log_button.php
@@ -2,35 +2,89 @@
    session_start();
    $root = basename($_SERVER["DOCUMENT_ROOT"]);
 
-   if(isset($_POST["logout"])){
+   if(isset($_GET["logout"])) {
+      session_unset();
+      session_destroy();
+      header("Location: " . $_GET["return"]);
+   }
+
+/*   if(isset($_POST["logout"])) {
       session_unset();
       session_destroy();
       header("Location: " . $_POST["return"]);
    }
-
-   if(isset($_SESSION["init"])){
+*/
+   if(isset($_SESSION["init"])) {
 ?>
 
+<style>
+   div.acc_menu {
+      position: fixed;
+      top: 10px;
+      right: 10px;
+      height: 15px;
+      width: 120px;
+      padding: 5px;
+      border-radius: 10px;
+      background-color: #FFF;
+   }
 
-<form id="log" style="position:fixed;top:10px;right:100px;" action="log_button.php" method="post">
-   <input type="hidden" name="logout" value="yes"/>
-   <input type="hidden" name="return" value="<?= $_SERVER['PHP_SELF'] ?>"/>
-   <a href="view_account.php"><?= $_SESSION["usernm"] ?></a> <input type="submit" value="logout"/>
-</form>
+   div.acc_menu ul {
+      margin: 0;
+      padding: 0;
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      list-style-type: none;
+   }
 
-<?php
-   } else {
-?>
+   div.acc_menu ul li {
+      display: block;
+      float: left;
+      height: auto;
+      width: 120px;
+   }
 
-<form id="something" style="position:fixed;top:10px;right:100px;" action="<?= 'login.php' ?>" method="post">
-   <input type="submit" value="Login"/>
-</form>
+   div.acc_menu ul ul {
+      display: none;
+      text-align: center;
+   }
+
+   div.acc_menu li:hover > ul {
+      display: block;
+   }
+
+   div.acc_menu ul ul li:hover {
+      position: relative;
+      float: none;
+      display: block;
+   }
+
+   div.acc_menu ul ul li {
+      background-color: #FFF;
+      border-radius: 10px;
+      border-top: 1px solid;
+   }
+</style>
+<div class="acc_menu">
+   <ul>
+      <li>
+         <a href="view_account.php"><?= $_SESSION["usernm"] ?></a>
+         <ul>
+            <li>
+               <a href="view_account.php?logout=yes&return=<?= $_SERVER['PHP_SELF'] ?>">Log out</a>
+               <!--<form id="log" action="log_button.php" method="post">
+                  <input type="hidden" name="logout" value="yes"/>
+                  <input type="hidden" name="return" value="<?= $_SERVER['PHP_SELF'] ?>"/>
+                  <input type="submit" value="logout"/>
+               </form>-->
+            </li>
+         </ul>
+      </li>
+   </ul>
+</div>
 
 <?php
    }
 ?>
-
-<form id="home" style="position:fixed;top:10px;right:10px;" action="index.php" method="post">
-    <input type="submit" value="Home"/>
-</form>
 
